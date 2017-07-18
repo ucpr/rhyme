19b5592b288fd6b18f13a2f8a70dad4b06596261
nve3pd <uchihara1229@gmail.com>
2017-07-19 00:01:18 +0900

    initial commit

---
+++ src/doc.py
@@ -0,0 +1,12 @@
+__doc__ = """{f}
+
+Usage:
+    rhyme.py [-s|--show] [<hiragana_word>]
+    rhyme.py [-w|--write] [<hiragana_word>]
+    rhyme.py [-h|--help]
+
+options:
+    -h --help   show this help message
+    -s --show   write to file and show rhyming words
+    -w --write  write to file
+""".format(f=__file__)
