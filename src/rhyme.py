f03497b9a5552128f4591461f361dd1b43d25f7c
nve3pd <uchihara1229@gmail.com>
2017-12-02 16:09:44 +0900

    pykakasiでローマ字に変換させるようにした & docoptを使うのをやめた

--- src/rhyme.py
+++ src/rhyme.py
@@ -3,117 +3,58 @@
 import os
 import sys
 import marisa_trie
-from docopt import docopt
+from pykakasi import kakasi
 
-from words import roman_dict as rd
-from doc import __doc__
-
-HIRAGANA = "ぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとど \
-        なにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんー"
-IGNORE = "っ"
-PETIT = "ぁぃぅぇぉっゃゅょゎ"
+kakasi_py = kakasi()
+kakasi_py.setMode('H', 'a')
+kakasi_py.setMode('K', 'a')
+kakasi_py.setMode('J', 'a')
+conv = kakasi_py.getConverter()
 
 
-def _is_hiragana(s):
-    return all([ch in HIRAGANA for ch in s])
-
-
-def _is_connectable(c):
-    return c not in IGNORE and c in PETIT
-
-
-def _split_mora(word):
-    mora_list = []
-    length = len(word)
-
-    i = 0
-    while i < length:
-        if i + 1 < length:
-            if _is_connectable(word[i + 1]):
-                mora_list.append(word[i] + word[i + 1])
-                i += 2
-            else:
-                mora_list.append(word[i])
-                i += 1
-        else:
-            mora_list.append(word[i])
-            i += 1
-
-    return mora_list
+def _roman_str(text):
+    """ 与えられた文字列をローマ字に変換して返す """
+    return conv.do(text)
 
 
 def _vowel_str(word):
-    vowel_list = []
+    """ 母音のみの文字列にして返す """
+    vowel_str = ""
+    for i in _roman_str(word):
+        if i in "aiueon-":
+            vowel_str += i
 
-    for i in _split_mora(word):
-        vowel = rd[i][-1]
-        if i not in IGNORE and vowel in "aiueon":
-            vowel_list.append(vowel)
-        elif vowel == "-":
-            vowel_list.append(vowel_list[-1])
-
-    return "".join(vowel_list)
+    return vowel_str
 
 
-def _make_rhyme_words(w, state=0):
+def _make_rhyme_words(w):
+    """ 韻を踏んでいそうな単語を探索してリストで返す """
     files = os.listdir("./dictionary/")
     vowel_word = _vowel_str(w)
     vowel_length = len(vowel_word)
+    rhyme_words = []
 
     for i in files:
         with open("./dictionary/" + i, "r") as f:
             trie = marisa_trie.Trie(list(f))
-            if not os.path.isdir("../rhyme_words"):
-                os.mkdir("../rhyme_words")
+            res = trie.keys(vowel_word)
+            print(res)
 
-            with open("../rhyme_words/" + i[0] + ".txt", "w") as new_f:
-                res = trie.keys(vowel_word)
-                for r in res:
-                    tmp = r.split()
-                    if vowel_length < len(tmp[0]):
-                        continue
-                    new_f.write(tmp[1] + "\n")
-                if not state:
-                    print("add ./../rhyme_words/{}".format(i[0] + ".txt"))
+            for j in res:
+                tmp = j.split()
+                if len(tmp[0]) == vowel_length:  # 母音が全一致の場合のみ
+                    rhyme_words.append(tmp[1])
+
+    return rhyme_words
 
 
-def _input_word(args):
-    word = args["<hiragana_word>"]
-    if not word:
-        print("Error : 文字列が空文字列です.")
-        sys.exit()
-    if not _is_hiragana(word):
-        print("Error : ひらがなではありません.")
-        sys.exit()
-    return word
-
-
-def rhyme():
-    args = docopt(__doc__)
-
-    if args["--show"]:
-        word = _input_word(args)
-        _make_rhyme_words(word, 1)
-        files = os.listdir("../rhyme_words")
-        for f in files:
-            with open("../rhyme_words/" + f, "r", encoding="utf-8") as txt:
-                for row in txt:
-                    print(row.rstrip())
-    elif args["--write"]:
-        word = _input_word(args)
-        _make_rhyme_words(word)
-    elif args["--help"]:
-        print(__doc__)
-    else:
-        word = args["<hiragana_word>"]
-        if not word:
-            print(__doc__)
-        else:
-            if not _is_hiragana(word):
-                print("Error : ひらがなではありません.")
-                sys.exit()
-            _make_rhyme_words(word)
+def rhyme(text):
+    """ 韻を踏んでいそうな単語をリストで返す """
+    roman = _roman_str(text)
+    print(roman)
+    return _make_rhyme_words(roman)
 
 
 if __name__ == "__main__":
-    rhyme()
+    res = rhyme(sys.argv[1] if len(sys.argv) >= 2 else "")
+    print(res)
