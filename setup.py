9ec58e1cf6e0170fc4a3fd767d70b691f3d7328f
nve3pd <uchihara1229@gmail.com>
2018-04-03 00:50:30 +0900

    [add] initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='rhyme',
+    version='1.0.0',
+    description='I want to rhyme.',
+    author='nve3pd',
+    author_email='uchihara1229@gmail.com',
+    url='https://github.com/nve3pd/rhyme',
+    keywords = ["python3", "rhyme"],
+    install_requires=['marisa-trie', 'pykakasi'],
+    py_modules=['rhyme'],
+)
